860d40cc3576071a09d6d8cd92a9d91c87a7ad70
rollrat <rollrat.cse@gmail.com>
2021-12-11 23:09:58 +0000

    project: remove firebase modules

--- android/app/src/main/kotlin/com/example/communityexplorer/Application.kt
+++ android/app/src/main/kotlin/com/example/communityexplorer/Application.kt
@@ -4,16 +4,16 @@ import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
 import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+//import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
 
 class Application : FlutterApplication(), PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
-        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+        //FlutterFirebaseMessagingService.setPluginRegistrant(this)
     }
 
     override fun registerWith(registry: PluginRegistry) {
-        registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin")
+        //registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin")
 //        GeneratedPluginRegistrant.registerWith(registry)
     }
 }
